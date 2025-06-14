c75ee5427354f0c24049afe9ae060fafdcd63357
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-14 19:00:35 -0300

    feat(telemetry): started to implement an integration with Prometheus

---
+++ internal/telemetry/prometheus.go
@@ -0,0 +1,20 @@
+package telemetry
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// StartPrometheusEndpoint
+// Starts an HTTP server at /metrics
+func StartPrometheusEndpoint(port string) {
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		log.Printf("Serving Prometheus metrics at http://localhost:%s/metrics\n", port)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatalf("failed to start Prometheus HTTP server: %v", err)
+		}
+	}()
+}
