a11364637e4197484889d6eb65c9d525f6555437
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-14 16:13:16 -0300

    feat(shared): added a 'Price' value object

---
+++ internal/domain/shared/price.go
@@ -0,0 +1,35 @@
+package shared
+
+import (
+	"errors"
+
+	"github.com/shopspring/decimal"
+)
+
+type Price struct {
+	value decimal.Decimal
+}
+
+func (p Price) Value() decimal.Decimal {
+	return p.value
+}
+
+func (p Price) String() string {
+	return p.value.StringFixed(2)
+}
+
+func NewPrice(amount string) (Price, error) {
+	// Parse the amount as a decimal
+	d, err := decimal.NewFromString(amount)
+
+	if err != nil {
+		return Price{}, errors.New("NewPrice(): invalid amount")
+	}
+
+	// Verify that the amount is greater than zero
+	if d.LessThanOrEqual(decimal.Zero) {
+		return Price{}, errors.New("NewPrice(): amount must be positive")
+	}
+
+	return Price{value: d}, nil
+}
