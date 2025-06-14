f3a3197fa1fe046aace939eb8b924b3ca041decc
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-14 17:13:54 -0300

    feat(customer): added a 'Customer' domain

---
+++ internal/domain/customer/customer.go
@@ -0,0 +1,39 @@
+package customer
+
+import (
+	"fmt"
+	"otel-prometheus-study/internal/domain/shared"
+)
+
+type Customer struct {
+	id   shared.ID
+	name shared.Name
+}
+
+func (c Customer) ID() int {
+	return c.id.Value()
+}
+
+func (c Customer) Name() string {
+	return c.name.Value()
+}
+
+func (c Customer) String() string {
+	return fmt.Sprintf("{'id': '%d', 'name': '%s'}", c.id.Value(), c.name.Value())
+}
+
+func NewCustomer(inputId int, inputName string) (Customer, error) {
+	// Check if inputId is a valid ID
+	id, err := shared.NewID(inputId)
+	if err != nil {
+		return Customer{}, err
+	}
+
+	// Check if inputName is a valid name
+	name, err := shared.NewName(inputName)
+	if err != nil {
+		return Customer{}, err
+	}
+
+	return Customer{id, name}, nil
+}
