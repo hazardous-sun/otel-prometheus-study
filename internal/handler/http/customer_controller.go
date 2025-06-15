ea2397c82d4f607380e4dc52b816f65a6f8469ea
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-15 11:42:52 -0300

    feat(handler/http): added routes for the 'Customer' controller

--- internal/handler/http/customer_controller.go
+++ internal/handler/http/customer_controller.go
@@ -1,5 +1,19 @@
 package http
 
+import "github.com/gin-gonic/gin"
+
 type CustomerController struct {
+	// TODO : implement this
+}
+
+func (c *CustomerController) Create(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (c *CustomerController) List(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (c *CustomerController) Get(ctx *gin.Context) {
 	// TODO : implement this
 }
