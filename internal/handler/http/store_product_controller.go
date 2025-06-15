68fcd823ca9b5f3bd5b21019775b07970faea862
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-15 11:55:55 -0300

    feat(handler/http): added routes for the 'StoreProduct' controller

--- internal/handler/http/store_product_controller.go
+++ internal/handler/http/store_product_controller.go
@@ -1,5 +1,23 @@
 package http
 
+import "github.com/gin-gonic/gin"
+
 type StoreProductController struct {
+	// TODO : implement this
+}
+
+func (s *StoreProductController) Create(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StoreProductController) List(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StoreProductController) Get(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StoreProductController) Update(ctx *gin.Context) {
 	// TODO : implement this
 }
