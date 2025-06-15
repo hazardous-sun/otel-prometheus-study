076e8b239efcb340ce3459fe6acdb4d60bfbf1da
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-15 11:48:28 -0300

    feat(handler/http): added routes for the 'Stock' controller

--- internal/handler/http/stock_controller.go
+++ internal/handler/http/stock_controller.go
@@ -1,5 +1,27 @@
 package http
 
+import "github.com/gin-gonic/gin"
+
 type StockController struct {
+	// TODO : implement this
+}
+
+func (s *StockController) Create(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StockController) List(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StockController) Get(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StockController) Update(ctx *gin.Context) {
+	// TODO : implement this
+}
+
+func (s *StockController) Delete(ctx *gin.Context) {
 	// TODO : implement this
 }
