c58de52657eb2fa5955280fe3d515bc9a413738b
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-06-15 16:06:23 -0300

    feat(infra/postgres): added a 'Product' repository layer

---
+++ internal/infra/postgres/product_repository.go
@@ -0,0 +1,63 @@
+package postgres
+
+import (
+	"database/sql"
+	"otel-prometheus-study/internal/domain/product"
+	"otel-prometheus-study/internal/domain/shared"
+	"otel-prometheus-study/internal/logger"
+	"strings"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func (pr ProductRepository) InsertProduct(p product.Product) (product.Product, error) {
+	query := `INSERT INTO products (name, price) VALUES ($1, $2) RETURNING id`
+	name := strings.ToLower(p.Name())
+	price := p.PriceValue.String()
+
+	var id int
+	err := pr.connection.QueryRow(query, name, price).Scan(&id)
+	if err != nil {
+		logger.LogError(err, "context", "InsertProduct")
+		return product.Product{}, err
+	}
+
+	newID, err := shared.NewID(id)
+	if err != nil {
+		return product.Product{}, err
+	}
+	p.IDValue = newID
+	return p, nil
+}
+
+func (pr ProductRepository) GetProducts() ([]product.Product, error) {
+	query := `SELECT id, name, price FROM products`
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		logger.LogError(err, "context", "GetProducts")
+		return nil, err
+	}
+	defer rows.Close()
+
+	var list []product.Product
+	for rows.Next() {
+		var id int
+		var name, price string
+		if err := rows.Scan(&id, &name, &price); err != nil {
+			return nil, err
+		}
+
+		prod, err := product.NewProduct(id, name, price)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, prod)
+	}
+	return list, nil
+}
+
+func NewProductRepository(db *sql.DB) ProductRepository {
+	return ProductRepository{connection: db}
+}
