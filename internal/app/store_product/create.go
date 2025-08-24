43c3ada272e62c559e06c554bb08f6554e0c7a18
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 20:00:09 -0300

    feat(internal/app/store_product): add method to create a new store product

--- internal/app/store_product/create.go
+++ internal/app/store_product/create.go
@@ -1,3 +1,22 @@
 package store_product
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/store_product"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func CreateStoreProduct(storeID int, productID int, price string, quantity int) (store_product.StoreProduct, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return store_product.StoreProduct{}, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStoreProductRepository(db)
+	newStoreProduct, err := store_product.NewStoreProduct(storeID, productID, price, quantity)
+	if err != nil {
+		return store_product.StoreProduct{}, err
+	}
+
+	return repo.InsertStoreProduct(newStoreProduct)
+}
