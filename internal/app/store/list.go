0d54f9338bbadb9f861a31b7f1e31e72d4401803
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:59:19 -0300

    feat(internal/app/store): add method to list all stores

--- internal/app/store/list.go
+++ internal/app/store/list.go
@@ -1,3 +1,17 @@
 package store
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/store"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func ListStores() ([]store.Store, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStoreRepository(db)
+	return repo.GetStores()
+}
