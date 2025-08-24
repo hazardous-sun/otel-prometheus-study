33e4e995d48e2826c7392f3c555fb5ba5cf9b9c4
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:57:29 -0300

    feat(internal/app/product): add method to list all products

--- internal/app/product/list.go
+++ internal/app/product/list.go
@@ -1,3 +1,17 @@
 package product
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/product"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func ListProducts() ([]product.Product, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewProductRepository(db)
+	return repo.GetProducts()
+}
