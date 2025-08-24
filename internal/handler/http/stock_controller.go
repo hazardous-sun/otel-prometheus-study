6b7c9c215d4b0e73f82304060d8a44f3c644fe8e
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 20:17:26 -0300

    feat(internal/handler): implement stock controller

--- internal/handler/http/stock_controller.go
+++ internal/handler/http/stock_controller.go
@@ -2,30 +2,45 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"otel-prometheus-study/internal/app/stock"
 	"otel-prometheus-study/internal/logger"
 )
 
 type StockController struct {
-	// TODO : implement this
-}
-
-func (s *StockController) Create(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StockController) List(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StockController) Get(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StockController) Update(ctx *gin.Context) {
-	// TODO : implement this
 }
 
 func NewStockController() StockController {
 	logger.LogInfo("initializing stock controller")
 	return StockController{}
+}
+
+func (sc *StockController) Create(c *gin.Context) {
+	var json struct {
+		ProductID int `json:"product_id"`
+		Quantity  int `json:"quantity"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newStock, err := stock.CreateStock(json.ProductID, json.Quantity)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newStock)
+}
+
+func (sc *StockController) List(c *gin.Context) {
+	stocks, err := stock.ListStocks()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, stocks)
 }
