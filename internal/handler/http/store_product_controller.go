250c72e67408585326eeb91f485eba0d542dfb0e
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 20:17:59 -0300

    feat(internal/handler): implement store product controller

--- internal/handler/http/store_product_controller.go
+++ internal/handler/http/store_product_controller.go
@@ -2,30 +2,47 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"otel-prometheus-study/internal/app/store_product"
 	"otel-prometheus-study/internal/logger"
 )
 
 type StoreProductController struct {
-	// TODO : implement this
-}
-
-func (s *StoreProductController) Create(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StoreProductController) List(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StoreProductController) Get(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (s *StoreProductController) Update(ctx *gin.Context) {
-	// TODO : implement this
 }
 
 func NewStoreProductController() StoreProductController {
 	logger.LogInfo("initializing store product controller")
 	return StoreProductController{}
+}
+
+func (spc *StoreProductController) Create(c *gin.Context) {
+	var json struct {
+		StoreID   int    `json:"store_id"`
+		ProductID int    `json:"product_id"`
+		Price     string `json:"price"`
+		Quantity  int    `json:"quantity"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newStoreProduct, err := store_product.CreateStoreProduct(json.StoreID, json.ProductID, json.Price, json.Quantity)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newStoreProduct)
+}
+
+func (spc *StoreProductController) List(c *gin.Context) {
+	storeProducts, err := store_product.ListStoreProducts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, storeProducts)
 }
