18868d5aa830b242c8433d34676890e55e248f99
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:58:25 -0300

    feat(internal/app/stock): add method to list all stocks

--- internal/app/stock/list.go
+++ internal/app/stock/list.go
@@ -1,3 +1,17 @@
 package stock
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/stock"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func ListStocks() ([]stock.Stock, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStockRepository(db)
+	return repo.GetStocks()
+}
