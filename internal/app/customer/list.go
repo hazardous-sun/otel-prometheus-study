258128fb7c5c0844a06477c70d16a15893bb0df2
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:56:29 -0300

    feat(internal/app/customer): add method to list all customers

--- internal/app/customer/list.go
+++ internal/app/customer/list.go
@@ -1,3 +1,17 @@
 package customer
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/customer"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func ListCustomers() ([]customer.Customer, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewCustomerRepository(db)
+	return repo.GetCustomers()
+}
