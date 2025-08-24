69408cab22eaeab24ca8ce7d48658df4c0fcf69d
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 20:00:42 -0300

    feat(internal/app/store_product): add method to list all store products

--- internal/app/store_product/list.go
+++ internal/app/store_product/list.go
@@ -1,3 +1,17 @@
 package store_product
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/store_product"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func ListStoreProducts() ([]store_product.StoreProduct, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStoreProductRepository(db)
+	return repo.GetStoreProducts()
+}
