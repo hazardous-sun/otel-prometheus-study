6c7c7e01d0650c76d0b6524bda0ef0c411d08fa9
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:58:01 -0300

    feat(internal/app/stock): add method to create a new stock

--- internal/app/stock/create.go
+++ internal/app/stock/create.go
@@ -1,3 +1,22 @@
 package stock
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/stock"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func CreateStock(productID int, quantity int) (stock.Stock, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return stock.Stock{}, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStockRepository(db)
+	newStock, err := stock.NewStock(0, productID, quantity)
+	if err != nil {
+		return stock.Stock{}, err
+	}
+
+	return repo.InsertStock(newStock)
+}
