907ce164c400f0af0e83d8f38268f79d9fc73e47
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:55:17 -0300

    feat(internal/app/customer): add method to create a new customer

--- internal/app/customer/create.go
+++ internal/app/customer/create.go
@@ -1,3 +1,22 @@
 package customer
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/customer"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func CreateCustomer(name string) (customer.Customer, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return customer.Customer{}, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewCustomerRepository(db)
+	newCustomer, err := customer.NewCustomer(0, name)
+	if err != nil {
+		return customer.Customer{}, err
+	}
+
+	return repo.InsertCustomer(newCustomer)
+}
