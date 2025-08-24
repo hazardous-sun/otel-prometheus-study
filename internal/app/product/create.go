df2cb77c3617923ca4d6e92a18c89a2d2ed6999a
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:57:02 -0300

    feat(internal/app/product): add method to create a new product

--- internal/app/product/create.go
+++ internal/app/product/create.go
@@ -1,3 +1,22 @@
 package product
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/product"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func CreateProduct(name string, price string) (product.Product, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return product.Product{}, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewProductRepository(db)
+	newProduct, err := product.NewProduct(0, name, price)
+	if err != nil {
+		return product.Product{}, err
+	}
+
+	return repo.InsertProduct(newProduct)
+}
