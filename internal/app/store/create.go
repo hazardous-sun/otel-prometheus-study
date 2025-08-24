cbd9934db4c0f72d4d3550c7ca679f72a8b1193f
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 19:59:01 -0300

    feat(internal/app/store): add method to create a new store

--- internal/app/store/create.go
+++ internal/app/store/create.go
@@ -1,3 +1,22 @@
 package store
 
-// TODO : implement this
+import (
+	"otel-prometheus-study/internal/domain/store"
+	"otel-prometheus-study/internal/infra/postgres"
+)
+
+func CreateStore(name string) (store.Store, error) {
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		return store.Store{}, err
+	}
+	defer db.Close()
+
+	repo := postgres.NewStoreRepository(db)
+	newStore, err := store.NewStore(0, name)
+	if err != nil {
+		return store.Store{}, err
+	}
+
+	return repo.InsertStore(newStore)
+}
