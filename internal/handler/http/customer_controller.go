d4077d837001a07cf669ab6525f4c72be9acda01
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-08-24 20:16:23 -0300

    feat(internal/handler): implement customer controller

--- internal/handler/http/customer_controller.go
+++ internal/handler/http/customer_controller.go
@@ -2,26 +2,44 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"otel-prometheus-study/internal/app/customer"
 	"otel-prometheus-study/internal/logger"
 )
 
 type CustomerController struct {
-	// TODO : implement this
-}
-
-func (c *CustomerController) Create(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (c *CustomerController) List(ctx *gin.Context) {
-	// TODO : implement this
-}
-
-func (c *CustomerController) Get(ctx *gin.Context) {
-	// TODO : implement this
 }
 
 func NewCustomerController() CustomerController {
 	logger.LogInfo("initializing customer controller")
 	return CustomerController{}
+}
+
+func (cc *CustomerController) Create(c *gin.Context) {
+	var json struct {
+		Name string `json:"name"`
+	}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newCustomer, err := customer.CreateCustomer(json.Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newCustomer)
+}
+
+func (cc *CustomerController) List(c *gin.Context) {
+	customers, err := customer.ListCustomers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, customers)
 }
